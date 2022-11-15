2a67176e32b1164795245d9b705f59f9acc38d8a
Lukasz-Lipinski <l.lipinski1993@gmail.com>
2022-11-15 13:54:01 +0100

    cors lib added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+let cors = require('cors');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
